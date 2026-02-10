8fcd8276bc110a50f7b0eeb9f948386d9c0bbe77
Azzazy6269 <mohamadelazzazy@gmail.com>
2026-02-10 23:25:10 +0200

    update follow system

--- Project/routers/users.js
+++ Project/routers/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const usersController = require('../controllers/users');
+const followsController = require('../controllers/follows');
 const schemas = require('../schemas/users');
+const followSchemas = require('../schemas/follows');
 const validate = require('../middlewares/validate');
 const authenticate = require('../middlewares/authenticate');
 const restrictToRolesorOwner = require('../middlewares/restrictToRolesorOwner');
@@ -10,6 +12,10 @@ const imageKit = require('../middlewares/upload');
 const router = express.Router();
 
 
+///users/search?name=:ammadibrahe //a part of the required name
+///users/search?email=:mohamed //whole the email
+router.get('/search',authenticate ,validate(schemas.searchUsersSchema), usersController.searchUsers);
+
 //forgot-password
 router.post('/forgot-password' , validate(schemas.forgotPasswordSchema) , usersController.forgotPassword);
 
@@ -17,7 +23,7 @@ router.post('/forgot-password' , validate(schemas.forgotPasswordSchema) , usersC
 router.post('/reset-password/:token' , validate(schemas.resetPasswordSchema) , usersController.resetPassword);
 
 //change-password
-router.post('/change-password' , authenticate , validate(schemas.changePasswordSchema) , usersController.changePassword);
+router.patch('/change-password' , authenticate , validate(schemas.changePasswordSchema) , usersController.changePassword);
 
 //upload profile picture
 router.post('/profile-picture',authenticate , imageKit.uploadProfile , usersController.uploadProfileImage);
@@ -35,8 +41,27 @@ router.post('/sign-up', validate(schemas.signUpSchema), usersController.signUp);
 router.post('/sign-in',validate(schemas.signInSchema), usersController.signIn)
 
 // get all users
-
 router.get('/', authenticate , restrictToRolesOnly.restrictTo(['admin']) , validate(schemas.getAllUsersSchema), usersController.getAllUsers);
+
+//follow user
+//POST /users/:userId/follow
+router.post('/:userId/follow' ,authenticate, validate(followSchemas.followUserSchema) , followsController.followUser);
+
+//unfollow user
+//DELETE /users/:userId/unfollow
+router.delete('/:userId/unfollow' , authenticate , validate(followSchemas.unfollowUserSchema) , followsController.unfollowUser);
+
+//get all followers
+//GET /users/:userId/followers
+router.get('/:userId/followers' , authenticate , validate(followSchemas.getAllFollowersSchema) , followsController.getAllFollowers);
+
+//get all followering
+//GET /users/:userId/following
+router.get('/:userId/following' , authenticate,validate(followSchemas.getAllFollowingsSchema) , followsController.getAllFollowing);
+
+//is follow
+//GET /users/:userId/isfollow
+router.get('/:userId/isfollow' , authenticate,validate(followSchemas.isFollowSchema) , followsController.isFollow);
 
 // get user by id
 router.get('/:id', authenticate , restrictToRolesorOwner.restrictTo(['admin']) , usersController.getUserById);
---
+++ Project/services/follows.js
@@ -0,0 +1,50 @@
+const Follow = require('../models/follows');
+const User = require('../models/users');
+const APIError = require('../utils/APIError');
+
+
+const followUser = async(followerId,followingId)=>{
+    if (followerId.toString() === followingId.toString()) {
+        throw new Error("You cannot follow yourself"); 
+    }
+    const follower = await User.findById(followerId);
+    if(!follower){
+        return {followLog : null ,message:"follower not found"};
+    }
+    const following = await User.findById(followingId);
+    if(!following){
+        return {followLog : null ,message:"following not found"};
+    }
+    const follow = await Follow.create({followerId,followingId});
+    return {followLog : follow ,message:"successful proccess"};
+}
+
+const unfollowUser = async(followerId,followingId)=>{
+    const unfollow = await Follow.findOneAndDelete({followerId,followingId});
+    if(!unfollow){
+        return {message:`unfollow failed`,unfollow};
+    }
+    return {message:`unfollow successful`,unfollow};
+}
+
+const getAllFollowers = async(followingId)=>{
+    const followers =await Follow.find({followingId}).populate('followerId','name email');
+    return {followers,message:"followers fetched successfully"};
+}
+
+const getAllFollowing = async(followerId)=>{
+    const followings = await Follow.find({followerId}).populate('followingId','name email');
+    return {followings , message : "followings fetched successfully"};
+}
+
+const isFollow = async(followerId,followingId)=>{
+    const follow = await Follow.findOne({followerId,followingId});
+    if(follow){
+        return true;
+    }
+    return false;
+}
+
+module.exports = {
+    followUser , unfollowUser , getAllFollowers , getAllFollowing , isFollow
+}
